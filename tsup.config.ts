b6ea09113ea5cb38f8cf6e9482fbe6c7227c8fb8
Conor Roberts <Conor@ConorRoberts.com>
2024-08-15 13:54:09 -0400

    Autocommit

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,12 +6,9 @@ export default defineConfig({
   dts: true,
   clean: true,
   entry: {
-    schema: "src/schema.ts",
-    db: "src/db.ts",
     cache: "src/cache.ts",
     logger: "src/logger.ts",
     env: "src/env.ts",
     images: "src/images.ts",
-    migrate: "src/migrate.ts",
   },
 });
