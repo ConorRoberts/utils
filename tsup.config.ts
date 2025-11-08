af15f4d8ddbe55c80b980f8ad88cfe13b578f877
Conor Roberts <Conor@ConorRoberts.com>
2025-11-07 20:25:16 -0500

    Autocommit

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  format: ["esm", "cjs"],
+  format: ["esm"],
   sourcemap: true,
   dts: true,
   clean: true,
   entry: {
-    schema: "src/schema.ts",
-    db: "src/db.ts",
-    cache: "src/cache.ts",
-    logger: "src/logger.ts",
     env: "src/env.ts",
     images: "src/images.ts",
-    migrate: "src/migrate.ts",
   },
 });
