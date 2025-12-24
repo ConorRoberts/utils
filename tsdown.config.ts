2a61292b97b55c9985f5d03cc71c8020785c4bc9
Conor Roberts <Conor@ConorRoberts.com>
2025-12-23 20:28:58 -0500

    Autocommit

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -11,17 +11,6 @@ export default defineConfig({
     images: "src/images.ts",
     react: "src/react/index.ts",
     "oxlint/index": "src/oxlint-plugins/index.js",
-    "oxlint/jsx-component-pascal-case": "src/oxlint-plugins/jsx-component-pascal-case.js",
-    "oxlint/no-component-date-instantiation": "src/oxlint-plugins/no-component-date-instantiation.js",
-    "oxlint/no-emoji": "src/oxlint-plugins/no-emoji.js",
-    "oxlint/no-finally": "src/oxlint-plugins/no-finally.js",
-    "oxlint/no-function-call-in-jsx": "src/oxlint-plugins/no-function-call-in-jsx.js",
-    "oxlint/no-inline-components": "src/oxlint-plugins/no-inline-components.js",
-    "oxlint/no-react-namespace": "src/oxlint-plugins/no-react-namespace.js",
-    "oxlint/no-switch-plugin": "src/oxlint-plugins/no-switch-plugin.js",
-    "oxlint/no-top-level-let": "src/oxlint-plugins/no-top-level-let.js",
-    "oxlint/no-type-cast": "src/oxlint-plugins/no-type-cast.js",
-    "oxlint/pretty-props": "src/oxlint-plugins/pretty-props.js",
   },
   onSuccess: async () => {
     const targetDir = "dist/oxlint";
